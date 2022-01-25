284f722c01bdc414384b9c7e9fe9a4dbc5dd0af9
João Jacques <joao@ommed.com>
2022-01-25 12:55:28 -0300

    Componente Tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from 'next'
 import Layout from '../components/Layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
 
 const Home: NextPage = () => {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Joao', 24, '2'),
+    new Cliente('Pedro', 12, '3'),
+    new Cliente('Maria', 22, '4'),
+  ]
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -9,7 +18,7 @@ const Home: NextPage = () => {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   )
